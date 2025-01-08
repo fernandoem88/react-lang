38b2d51da365f787010067378e0c27ce6bc6e85a
Fernando EM <vs_fernando@yahoo.fr>
2025-01-08 09:29:39 +0100

    clean up

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,40 +1,27 @@
 const typescript = require("rollup-plugin-typescript2");
 
-/*
-"size-limit": [
-  {
-    "path": "dist/react-lang.cjs.production.min.js",
-    "limit": "10 KB"
-  },
-  {
-    "path": "dist/react-lang.esm.js",
-    "limit": "10 KB"
-  }
-]
-*/
-
 module.exports = {
   input: {
-    index: 'src/index.ts',  // Entry for the index library
-     configure: 'src/configure.ts',  // Entry for the configure module
+    index: 'src/index.ts',  
+    configure: 'src/configure.ts',  
   },
   output: [
     {
       dir: 'dist',
       format: 'esm',
       sourcemap: true,
-      entryFileNames: '[name].js', // Output each entry as a separate file
+      entryFileNames: '[name].js',
     },
     {
       dir: 'dist',
       format: 'cjs',
       sourcemap: true,
-      entryFileNames: '[name].cjs', // Separate output for CommonJS
+      entryFileNames: '[name].cjs',
     }
   ],
   plugins: [typescript({
     tsconfig: './tsconfig.json',
-    clean: true, // Clear cache
+    clean: true, 
   })],
-  external: ["react", "fs", "path"], // Prevent bundling React
+  external: ["react", "fs", "path"],
 };
