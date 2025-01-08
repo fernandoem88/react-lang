730c917910dad94d2792a583b7fd2f306cbffbe6
Fernando EM <vs_fernando@yahoo.fr>
2025-01-08 08:14:31 +0100

    done

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
-import typescript from "rollup-plugin-typescript2";
+const typescript = require("rollup-plugin-typescript2");
 
-const index = {
+module.exports = {
   input: {
     index: 'src/index.ts',  // Entry for the index library
+     configure: 'src/configure.ts',  // Entry for the configure module
   },
   output: [
     {
@@ -23,65 +24,4 @@ const index = {
     clean: true, // Clear cache
   })],
   external: ["react", "fs", "path", "uuid"], // Prevent bundling React
-}
-
-const configure = {
-  input: {
-    configure: 'src/configure.ts',  // Entry for the configure module
-  },
-  output: [
-    {
-      dir: 'dist',
-      format: 'cjs',
-      sourcemap: true,
-      entryFileNames: '[name].cjs.js', // Separate output for CommonJS
-    }
-  ],
- 
-  plugins: [typescript({
-    tsconfig: './tsconfig.json',
-    clean: true, // Clear cache
-  })],
-  external: ["react", "fs", "path", "uuid"], // Prevent bundling React
-}
-
-export default [
-  index, configure
-]
-
-// export default {
-//   // input: "src/index.ts",
-//   input: {
-//     index: 'src/index.ts',  // Entry for the index library
-//     // configure: 'src/configure.ts',  // Entry for the configure module
-//   },
-//   output: [
-//     {
-//       dir: 'dist',
-//       format: 'esm',
-//       sourcemap: true,
-//       entryFileNames: '[name].js', // Output each entry as a separate file
-//     },
-//     {
-//       dir: 'dist',
-//       format: 'cjs',
-//       sourcemap: true,
-//       entryFileNames: '[name].cjs.js', // Separate output for CommonJS
-//     }
-//   ],
-//   // output: [
-//   //   {
-//   //     file: "dist/index.js",
-//   //     format: "cjs", // CommonJS
-//   //   },
-//   //   {
-//   //     file: "dist/index.esm.js",
-//   //     format: "esm", // ESModule
-//   //   },
-//   // ],
-//   plugins: [typescript({
-//     tsconfig: './tsconfig.json',
-//     clean: true, // Clear cache
-//   })],
-//   external: ["react", "fs", "path", "uuid"], // Prevent bundling React
-// };
+};
