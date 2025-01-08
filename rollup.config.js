ed17769249027f9934f9355f29350f23429d362b
Fernando EM <vs_fernando@yahoo.fr>
2025-01-08 07:29:18 +0100

    rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,8 @@
 import typescript from "rollup-plugin-typescript2";
 
-export default {
-  input: "src/index.ts",
+const indexConfig = {
   input: {
     index: 'src/index.ts',  // Entry for the index library
-    configure: 'src/configure.ts',  // Entry for the configure module
   },
   output: [
     {
@@ -20,24 +18,63 @@ export default {
       entryFileNames: '[name].cjs.js', // Separate output for CommonJS
     }
   ],
-//   output: [
-//     {
-//       file: "dist/index.js",
-//       format: "cjs", // CommonJS
-//     },
-//     {
-//       file: "dist/index.esm.js",
-//       format: "esm", // ESModule
-//     },
-//     {
-//         file: "dist/configure.js",
-//         format: "cjs", // CommonJS
-//     },
-//     {
-//         file: "dist/configure.esm.js",
-//         format: "esm", // ESModule
-//     },
-//   ],
+  plugins: [typescript({
+    tsconfig: './tsconfig.json',
+    clean: true, // Clear cache
+  })],
+  external: ["react", "fs", "path"], // Prevent bundling React
+}
+
+const configure = {
+  input: {
+    configure: 'src/configure.ts',  // Entry for the configure module
+  },
+  output: [
+    {
+      dir: 'dist',
+      format: 'cjs',
+      sourcemap: true,
+      entryFileNames: '[name].cjs.js', // Separate output for CommonJS
+    }
+  ],
+ 
+  plugins: [typescript({
+    tsconfig: './tsconfig.json',
+    clean: true, // Clear cache
+  })],
+  external: ["react", "fs", "path"], // Prevent bundling React
+}
+
+export default {
+  // input: "src/index.ts",
+  input: {
+    index: 'src/index.ts',  // Entry for the index library
+    // configure: 'src/configure.ts',  // Entry for the configure module
+  },
+  output: [
+    {
+      dir: 'dist',
+      format: 'esm',
+      sourcemap: true,
+      entryFileNames: '[name].js', // Output each entry as a separate file
+    },
+    {
+      dir: 'dist',
+      format: 'cjs',
+      sourcemap: true,
+      entryFileNames: '[name].cjs.js', // Separate output for CommonJS
+    }
+  ],
+  // output: [
+  //   {
+  //     file: "dist/index.js",
+  //     format: "cjs", // CommonJS
+  //   },
+  //   {
+  //     file: "dist/index.esm.js",
+  //     format: "esm", // ESModule
+  //   },
+  // ],
   plugins: [typescript({
     tsconfig: './tsconfig.json',
     clean: true, // Clear cache
